ea58c3f254df90ee3da95b869024f1c1e54318c8
AgoDe <ago.defelice@gmail.com>
2022-02-01 15:33:35 +0100

    fix: css input box in header

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -2,37 +2,41 @@
     <header>
         <img src="../assets/spotify-logo.png" alt="logo">
 
-        <label for="genre">GENRE</label>
-        <select name="genre" id=""
-        v-model="selectedGenre"
-        v-on:change="$emit('genreFilter', selectedGenre)"
-        >
-            <option disabled value="" >Filter by Genre</option>
-            <option value="">All</option>
-            <option
-            v-for="(item, index) in genre"
-            :key="index"
-            :value="item"
-            >
-            {{ item }}
-            </option>
-        </select>
+        <div class="input-box">
 
-        <label for="artist">ARTIST</label>
-        <select name="artist" id=""
-        v-model="selectedArtist"
-        v-on:change="$emit('artistFilter', selectedArtist)"
-        >
-            <option disabled value="" >Filter by Artist</option>
-            <option value="">All</option>
-            <option
-            v-for="(item, index) in artists"
-            :key="index"
-            :value="item"
+            <label for="genre">GENRE</label>
+            <select name="genre" id=""
+            v-model="selectedGenre"
+            v-on:change="$emit('genreFilter', selectedGenre)"
             >
-            {{ item }}
-            </option>
-        </select>
+                <option disabled value="" >Filter by Genre</option>
+                <option value="">All</option>
+                <option
+                v-for="(item, index) in genre"
+                :key="index"
+                :value="item"
+                >
+                {{ item }}
+                </option>
+             </select>
+
+            <label for="artist">ARTIST</label>
+            <select name="artist" id=""
+            v-model="selectedArtist"
+            v-on:change="$emit('artistFilter', selectedArtist)"
+            >
+                    <option disabled value="" >Filter by Artist</option>
+                    <option value="">All</option>
+                    <option
+                    v-for="(item, index) in artists"
+                    :key="index"
+                    :value="item"
+                    >
+                    {{ item }}
+                    </option>
+            </select>
+            </div>
+       
 
     </header>
 </template>
@@ -63,6 +67,9 @@ export default {
  header {
     height: 50px;
     background: rgb(53, 61, 71);
+
+    display: flex;
+    justify-content: space-between;
     
 
     img {
@@ -70,6 +77,21 @@ export default {
       margin: 10px;
       
     }
+    .input-box {
+        padding: 15px;
+        color: white;
+        
+
+        label {
+            margin-left: 20px;
+            margin-right: 5px;
+        }
+
+
+
+    }
+
+
   }
 
 </style>
