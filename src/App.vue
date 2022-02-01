2baba50b29a71f772dee251a2ffbff917e01eb4b
AgoDe <ago.defelice@gmail.com>
2022-02-01 15:26:21 +0100

    print:  genre select e artist select in header

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app">
    
     <!-- header -->
-    <header>
-      <img src="./assets/spotify-logo.png" alt="logo">
-    </header>
+    <header-container
+    @genreFilter="filterByGenre"
+    @artistFilter="filterByArtist"
+    :genre="musicalGenre"
+    :artists="musicalArtists"
+    />
     <!-- / header -->
 
     <!-- main -->
       <main-container
-      :disks="disks"
+      :disks="generalFilter"
       />
     <!-- / main -->
 
@@ -18,25 +21,63 @@
 
 <script>
 import axios from 'axios'
-import MainContainer from './components/MainContainer.vue'
+import MainContainer from './components/MainContainer.vue';
+import HeaderContainer from './components/HeaderContainer.vue';
 
 
 export default {
   name: 'App',
   components: {
     MainContainer,
+    HeaderContainer,
     
   },
   data() {
     return {
       disks: [],
+      disksSelected: [],
+      musicalGenre: [],
+      musicalArtists: [],
+      generalFilter: [],
+      
     }
+  },
+  methods: {
+    filterByGenre: function(selected) {
+
+      this.disksSelected = this.disks.filter((element) => {
+        return element.genre.includes(selected)
+      })
+      this.generalFilter = this.disksSelected;
+      const artistArray = this.disksSelected.map((element) => {
+        return element.author
+      })
+      this.musicalArtists = [...new Set(artistArray)];
+
+
+      
+    },
+
+    filterByArtist: function(selected) {
+
+      this.generalFilter = this.disksSelected.filter((element) => {
+        return element.author.includes(selected)
+      })
+    },
   },
   mounted() {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
-      this.disks = response.data.response
-      
-    })
+      this.disks = response.data.response;
+      this.generalFilter = response.data.response;
+
+      const genreArray = this.disks.map((element) => {
+        return element.genre
+      })
+      this.musicalGenre = [...new Set(genreArray)];
+
+    
+    });
+
   }
 }
 </script>
@@ -51,17 +92,7 @@ export default {
     font-family: 'Roboto', sans-serif;
   }
 
-  header {
-    height: 50px;
-    background: rgb(53, 61, 71);
-    
-
-    img {
-      height: 30px;
-      margin: 10px;
-      
-    }
-  }
+ 
 
 
 
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,75 @@
+<template>
+    <header>
+        <img src="../assets/spotify-logo.png" alt="logo">
+
+        <label for="genre">GENRE</label>
+        <select name="genre" id=""
+        v-model="selectedGenre"
+        v-on:change="$emit('genreFilter', selectedGenre)"
+        >
+            <option disabled value="" >Filter by Genre</option>
+            <option value="">All</option>
+            <option
+            v-for="(item, index) in genre"
+            :key="index"
+            :value="item"
+            >
+            {{ item }}
+            </option>
+        </select>
+
+        <label for="artist">ARTIST</label>
+        <select name="artist" id=""
+        v-model="selectedArtist"
+        v-on:change="$emit('artistFilter', selectedArtist)"
+        >
+            <option disabled value="" >Filter by Artist</option>
+            <option value="">All</option>
+            <option
+            v-for="(item, index) in artists"
+            :key="index"
+            :value="item"
+            >
+            {{ item }}
+            </option>
+        </select>
+
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+           selectedGenre: '',
+           selectedArtist: '',
+
+
+        }
+    },
+    props: {
+        genre: Array,
+        artists: Array,
+    },
+    methods: {
+      
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+ header {
+    height: 50px;
+    background: rgb(53, 61, 71);
+    
+
+    img {
+      height: 30px;
+      margin: 10px;
+      
+    }
+  }
+
+</style>
